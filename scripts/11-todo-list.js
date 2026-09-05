441f3425326da6ee8a076555e0dd28e3a9e133ad
Ojemola <ojemolae@gmail.com>
2026-09-05 18:56:41 +0100

    updated project

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,21 @@
+const todoList =  ['Make dinner', '', 'Wash dishes'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+function addTodo () {
+const inputElement = document.querySelector('.js-name-input'); 
+
+ const name = inputElement.value;
+
+todoList.push(name);
+console.log(todoList);
+
+inputElement.value = '';
+}
